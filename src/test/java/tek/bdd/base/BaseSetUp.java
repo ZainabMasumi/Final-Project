873b0c544788f7abdd89b2f42c3946b3e04b58da
zmasu <zmasumi@gmail.com>
2024-08-30 01:22:31 -0400

    set up

--- src/test/java/tek/bdd/base/BaseSetUp.java
+++ src/test/java/tek/bdd/base/BaseSetUp.java
@@ -1,4 +1,28 @@
 package tek.bdd.base;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
 public class BaseSetUp {
+    private static WebDriver driver;
+
+    public void setupBrowser() {
+    driver = new ChromeDriver();
+    driver.get("https://dev.insurance.tekschool-students.com/");
+    driver.manage().window().maximize();
+    driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+
+    }
+
+    public void quitBrowser() {
+        if (driver != null) {
+            driver.quit();
+
+        }
+    }
+    public WebDriver getDriver() {
+        return driver;
+    }
 }
